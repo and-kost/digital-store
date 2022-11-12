c801331bbdd82da8d079d6b59c8efd40d439ec65
andrey_kostyuchenko <andrey.kostyuchenko@dsr-corporation.com>
2022-11-12 14:08:27 +0300

    try validation with formik on add type dialog

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,6 +1,12 @@
 import React, {useState} from 'react';
-import {Button, Form, Modal} from 'react-bootstrap';
+import {Button, Form, Modal, Col} from 'react-bootstrap';
 import {createType} from '../../http/deviceApi';
+import { Formik  } from 'formik';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+    type: yup.string().required()
+});
 
 const CreateType = ({show, handleClose}) => {
     const [value, setValue] = useState('')
@@ -12,30 +18,55 @@ const CreateType = ({show, handleClose}) => {
         })
     }
 
-    return (
+    return <Formik
+        validationSchema={schema}
+        onSubmit={addType}
+        initialValues={{
+            type: ''
+        }}
+        >
+        {({
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              values,
+              touched,
+              isValid,
+              errors,
+          }) => (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>Add new type</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Form.Control
-                        value={value}
-                        onChange={e => setValue(e.target.value)}
-                        placeholder={'Enter type...'}
-                    />
+                <Form noValidate>
+                    <Form.Group as={Col} md="12" controlId="validationFormik03">
+                        <Form.Label>Type</Form.Label>
+                        <Form.Control
+                            value={values.type}
+                            type="text"
+                            name="type"
+                            onChange={(e) => {handleChange(e); setValue(e.target.value)}}
+                            placeholder={'Enter type...'}
+                            isInvalid={!!errors.type}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.type}
+                        </Form.Control.Feedback>
+                    </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     Close
                 </Button>
-                <Button variant="primary" onClick={addType}>
+                <Button variant="primary" onClick={handleSubmit}>
                     Submit
                 </Button>
             </Modal.Footer>
         </Modal>
-    );
+    )}
+    </Formik>
 };
 
 export default CreateType;
